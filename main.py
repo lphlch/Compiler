2d4b10d0bf515ff633df1735755da48b5e3a6a64
217729 <105117927+217729@users.noreply.github.com>
2022-11-12 16:33:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ inputs, keywordsList = getInput(1)  # input grammar
 grammar = transformInput(inputs)  # transform grammar to a list of productions
 firstSet = getFirstSet(grammar, keywordsList)  # get first set
 
+ACTION_GOTO,point_grammar=GET_ACTION_GOTO(firstSet,grammar,keywordsList) #get action_goto table&point_grammar is used to analyze
+
 getSentenceInput(1)  # input sentence, save to file sentence.txt
 lexResultList = callLex("sentence.txt")  # call Lex.exe to analyze input
 formalList = processLexResult(lexResultList)  # get formal sentence
