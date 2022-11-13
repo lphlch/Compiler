cc2f678a70b51048b5f1005be63f716a830596bf
lphlch <lvpinhao@sohu.com>
2022-11-13 20:29:37 +0800

    SentenceFormaler

--- SentenceFormaler.py
+++ SentenceFormaler.py
@@ -19,15 +19,18 @@ def getSentenceInput(method=0):
 
 def callLex(inputString):
     exeFile = "Lex.exe"
-    lexResultList = os.popen(exeFile + " " + inputString).read()
+    os.popen(exeFile + " " + inputString).read()
+    lexResultFile = open("lexical_analysis.txt", "r")
+    lexResultList = lexResultFile.read()
     return lexResultList
 
 def processLexResult(lexResultList):
     # ignore first line and last line
-    formalList = lexResultList.split("\n")[1:-1]
+    formalList = lexResultList.split("\n")[3:-1]
     resultList=[]
     for item in formalList:
-        item = item.split('\t')[1]
+        item = item.split()[0]
+        # print(item)
         resultList.append(item)
         # print(item.split('\t')[1])
         
@@ -37,6 +40,7 @@ def processLexResult(lexResultList):
     
     
     
-# getSentenceInput(1)
-# lexResultList=(callLex("sentence.txt"))
-# print(processLexResult(lexResultList))
+getSentenceInput(1)
+lexResultList=(callLex("sentence.txt"))
+# print(lexResultList)
+print(processLexResult(lexResultList))
