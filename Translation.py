af1500914dc072f0a488f303e9bb8b04c1187f4b
217729 <105117927+217729@users.noreply.github.com>
2023-01-02 22:45:43 +0800

    Add files via upload

--- Translation.py
+++ Translation.py
@@ -35,8 +35,8 @@ class TranslationProcess:
 
         self.codes = []
 
-        self.VALUEDIR = 3   # the position of values dict in the symbol stack
-        
+        self.VALUEDIR = 3  # the position of values dict in the symbol stack
+
         # the type of production
         self.EXPRESSION = [
             "expression",
@@ -46,6 +46,20 @@ class TranslationProcess:
             "primary_expression",
         ]
         self.OP_EXPRESSION = ["first_operator", "second_operator"]
+        self.M = [
+            "M",
+            "M_selection_statement",
+            "N_iteration_statement"
+        ]
+        self.N=[
+            "N_selection_statement",
+        ]
+        self.IF_EXPERSSION=[
+            "selection_statement",
+        ]
+        self.WHILE_EXPERSSION=[
+            "iteration_statement",
+        ]
         self.BOOL_EXPRESSION = [
             "or_bool_expression",
             "and_bool_expression",
@@ -67,12 +81,28 @@ class TranslationProcess:
         print("parentNodeStr", parentNodeStr)
         print("childrenNode", childrenNode)
 
-        if parentNodeStr in self.EXPRESSION:  # if is arth expression, do translation
+        if parentNodeStr in self.M:
+            parentValue = {"quad": str(len(self.codes))}
 
+        elif parentNodeStr in self.N:
+            parentValue={"nextlist":{str(len(self.codes))}}
+            code=["j","-","-","0"]
+            self.codes.append(code)
+
+        elif parentNodeStr in self.EXPRESSION:  # if is arth expression, do translation
             if (
-                len(childrenNode) == 1
+                    len(childrenNode) == 1
             ):  # if only one child, assign child value to parent
                 parentValue = childrenNode[0][self.VALUEDIR]
+            elif parentNodeStr=="third_expression" and childrenNode[0][0]=='!':
+                arg = childrenNode[1][self.VALUEDIR]
+                if(arg.get("falselist")==None):
+                    truelist={len(self.codes)+1}
+                    falselist={len(self.codes)}
+                    self.translate_id(arg)
+                    parentValue = {"truelist": truelist, "falselist": falselist}
+                else:
+                    parentValue = {"truelist": arg.get("falselist"), "falselist": arg.get("truelist")}
             else:  # if more than one child, do translation
                 op = childrenNode[1][self.VALUEDIR]
                 arg1 = childrenNode[0][self.VALUEDIR]
@@ -94,8 +124,86 @@ class TranslationProcess:
                 parentValue = childrenNode[0][self.VALUEDIR]
             else:
                 # if more than one child, do translation
-                ...
-                # todo: bool expression translation
+                if 'bool_operator' in childrenNode[1]:  # E->id1 relop id2
+                    op = childrenNode[1][self.VALUEDIR]
+                    arg1 = childrenNode[0][self.VALUEDIR]
+                    arg2 = childrenNode[2][self.VALUEDIR]
+                    parentValue = {"truelist": {len(self.codes)}, "falselist": {len(self.codes) + 1}}
+                    self.translate_relop(op, arg1, arg2)
+                elif 'and_operator' in childrenNode[1]:  # and语句的翻译
+                    quad = childrenNode[2][self.VALUEDIR]
+                    quad = quad.get("quad")
+                    arg1 = childrenNode[0][self.VALUEDIR]
+                    arg2 = childrenNode[3][self.VALUEDIR]
+                    if arg1.get("truelist")==None and arg2.get("truelist")==None:
+                        truelist1 = {len(self.codes)}
+                        falselist1 = {len(self.codes)+1}
+                        self.translate_id(arg1)
+                        backlist=truelist1
+                        quad=str(len(self.codes))
+                    elif arg1.get("truelist")==None:
+                        print("请不要输入类似a && E的形式，其中a是标识符！！！")
+                        exit(0)
+                    else:
+                        backlist = arg1.get("truelist")  # 要回填的链表
+                        falselist1=arg1.get("falselist")
+                    for item in backlist:  # 回填
+                        item = int(item)
+                        self.codes[item][3] = quad
+                    if arg2.get("truelist")==None:
+                        truelist2 = {len(self.codes)}
+                        falselist2 = {len(self.codes) + 1}
+                        self.translate_id(arg2)
+                    else:
+                        truelist2=arg2.get("truelist")
+                        falselist2=arg2.get("falselist")
+                    truelist = truelist2  # 获得实际真假链
+                    falselist = falselist1|falselist2
+                    parentValue = {"truelist": truelist, "falselist": falselist}
+                    quad=str(len(self.codes))
+                    for item in truelist:  # 回填
+                        item = int(item)
+                        self.codes[item][3] = quad
+                    for item in falselist:
+                        item = int(item)
+                        self.codes[item][3] = quad
+                elif 'or_operator' in childrenNode[1]:
+                    quad = childrenNode[2][self.VALUEDIR]
+                    quad = quad.get("quad")
+                    arg1 = childrenNode[0][self.VALUEDIR]
+                    arg2 = childrenNode[3][self.VALUEDIR]
+                    if arg1.get("truelist")==None and arg2.get("truelist")==None:
+                        truelist1 = {len(self.codes)}
+                        falselist1 = {len(self.codes)+1}
+                        self.translate_id(arg1)
+                        backlist=falselist1
+                    elif arg1.get("truelist")==None:
+                        print("请不要输入类似a || E的形式，其中a是标识符！！！")
+                        exit(0)
+                    else:
+                        backlist = arg1.get("falselist")  # 要回填的链表
+                        truelist1=arg1.get("truelist")
+                    for item in backlist:  # 回填
+                        item = int(item)
+                        self.codes[item][3] = quad
+                    if arg2.get("truelist")==None:
+                        truelist2 = {len(self.codes)}
+                        falselist2 = {len(self.codes) + 1}
+                        self.translate_id(arg2)
+                    else:
+                        truelist2=arg2.get("truelist")
+                        falselist2=arg2.get("falselist")
+                    truelist = truelist1 |truelist2
+                    falselist = falselist2
+                    parentValue = {"truelist": truelist, "falselist": falselist}
+                    quad = str(len(self.codes))
+                    for item in truelist:  # 回填
+                        item = int(item)
+                        self.codes[item][3] = quad
+                    for item in falselist:
+                        item = int(item)
+                        self.codes[item][3] = quad
+
 
         elif parentNodeStr in self.DECLARATION:
             # if is declaration, need to emit a assign code & add to symbol table
@@ -115,6 +223,79 @@ class TranslationProcess:
             newName = self.translateAssign(name, value)
             parentValue = {"identifierName": newName}
 
+        elif parentNodeStr=="bool_operator":
+            parentValue={"op":childrenNode[0][0]}
+
+        elif parentNodeStr in self.IF_EXPERSSION:
+            if len(childrenNode)==6:#不含else
+                E=childrenNode[2][self.VALUEDIR]
+                S=childrenNode[5][self.VALUEDIR]
+                quad=childrenNode[4][self.VALUEDIR].get("quad")
+                backlist=E.get("truelist")
+                for item in backlist:#回填
+                    item=int(item)
+                    self.codes[item][3]=quad
+                a=S.get("nextlist")
+                if S.get("nextlist")==None:
+                    a=set()
+                parentValue={"nextlist":E.get("falselist")|a}
+                quad=str(len(self.codes))
+                for item in E.get("falselist")|a:
+                    item=int(item)
+                    self.codes[item][3]=quad
+
+            else:#含else
+                E=childrenNode[2][self.VALUEDIR]
+                quad1=childrenNode[4][self.VALUEDIR].get("quad")
+                S1=childrenNode[5][self.VALUEDIR]
+                N=childrenNode[6][self.VALUEDIR]
+                quad2=childrenNode[8][self.VALUEDIR].get("quad")
+                S2=childrenNode[9][self.VALUEDIR]
+                backlist1=E.get("truelist")
+                for item in backlist1:#回填1
+                    item=int(item)
+                    self.codes[item][3]=quad1
+                backlist2=E.get("falselist")
+                for item in backlist2:#回填2
+                    item=int(item)
+                    self.codes[item][3]=quad2
+                a=S1.get("nextlist")
+                b=S2.get("nextlist")
+                if S1.get("nextlist")==None:
+                    a=set()
+                if S2.get("nextlist")==None:
+                    b=set()
+                parentValue={"nextlist":a|b|N.get("nextlist")}
+                quad = str(len(self.codes))
+                for item in a|b|N.get("nextlist"):
+                    item = int(item)
+                    self.codes[item][3] = quad
+
+        elif parentNodeStr in self.WHILE_EXPERSSION:#while
+            quad1=childrenNode[1][self.VALUEDIR].get("quad")
+            E=childrenNode[3][self.VALUEDIR]
+            quad2=childrenNode[5][self.VALUEDIR].get("quad")
+            S=childrenNode[6][self.VALUEDIR]
+            if S.get("nextlist")!=None:
+                backlist1=S.get("nextlist")
+            else:
+                backlist1=set()
+            for item in backlist1:
+                item=int(item)
+                self.codes[item][3]=quad1
+            backlist2=E.get("truelist")
+            for item in backlist2:
+                item=int(item)
+                self.codes[item][3]=quad2
+            parentValue={"nextlist":E.get("falselist")}
+            code=["j","-","-",quad1]
+            self.codes.append(code)
+            quad=str(len(self.codes))
+            for item in E.get("falselist"):
+                item=int(item)
+                self.codes[item][3]=quad
+
+
         # todo: add more translation function here
 
         print("return parentValue", parentValue)
@@ -122,10 +303,39 @@ class TranslationProcess:
 
     def genCode(self):
         print("--------------------")
+        num=0
         for code in self.codes:
-            print(code)
+            print(num,":",code)
+            num=num+1
 
-    #! deprecated
+    def translate_relop(self, op, arg1, arg2):#a>=b之类的翻译
+        if arg1.get("identifierName") != None:
+            arg1Val = arg1.get("identifierName")
+        else:
+            arg1Val = arg1.get("numberValue")
+
+        if arg2.get("identifierName") != None:
+            arg2Val = arg2.get("identifierName")
+        else:
+            arg2Val = arg2.get("numberValue")
+
+        code = ["j" + op.get("op"), arg1Val, arg2Val, "0"]
+        self.codes.append(code)
+        code = ["j", "-", "-", "0"]
+        self.codes.append(code)
+
+    def translate_id(self,arg):#a的布尔翻译（a 不是表达式）
+        if arg.get("identifierName") != None:
+            argVal = arg.get("identifierName")
+        else:
+            argVal = arg.get("numberValue")
+        code=["jnz",argVal,"-","0"]
+        self.codes.append(code)
+        code=["j","-","-","0"]
+        self.codes.append(code)
+
+
+    # ! deprecated
     # def translateDeclareConst(self, typeOfValue, name, value):
     #     # const int a = 1;
     #     # typeOfValue = int, name = a, value = 1
